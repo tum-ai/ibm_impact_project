9fee310af3828d1a76a22d8086f7d16783a4b639
Vincent Limbach <vincent.limbachsr@gmail.com>
2024-09-06 19:08:49 +0200

    Modified the tests to check for , and work with ####. The latter change can be undone but one must ensure that truly the last number is parsed completly

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -25,23 +25,33 @@ class TestEvaluationMethods(unittest.TestCase):
         cls.metrics_rt = CustomMetrics(cls.tokenizer_rt, number_encoding="rt", output_dir="tests/test_output")
 
     def test_calculate_result_mse(self):
+        #Important make sure that if you remove ### again that the model matches the last number and completely.
         labels = [
-            "First test 23.0 and -4.0",
-            "Is 29.0 - 478.2 = 34.452 correct?",
-            "Test text -34*65=78",
-            "Test 12-12 = 0 wrong?",
-            "Calculation: 12 + 12 = 24"
+            "Simple #### 5", #Parse positive 
+            "First test 23.0 and -4.0 #### -4.0", #Parse negative
+            "First test 23.0 and -4.0 #### -4.0", #Parse negative and positiv
+            "Is 29.0 - 478.2 = 34.452 correct? #### 34.452", #Parse decimal
+            "Test text -34*65=80 #### 78", #Focusses on last
+            "Test 12-12 = 0 wrong? #### 0", #Handles missing value gracefully
+            "Calculation: 12 + 12 = 24 #### 78", #Handels missing marker gracefully
+            "Testing long 19,202 #### 19,202", #Handels , 
+            "Testing long 19,203,023 #### 19,203,023", #Can handle two ,,
+            #Current logic doesn't handle 123,21 correctly makes it 12321 instead of throwing error
         ]
         predictions = [
-            "First test 23.0 and -1.0",
-            "Is 29.0 - 478.2 = 34.452 correct?",
-            "Test text -34*65=80",
-            "Test 12-12 = -1 wrong?",
-            "Calculation: calculation calculation"
+            "Simple #### 7",
+            "First test 23.0 and -1.0 #### -1.0",
+            "First test 23.0 and -1.0 #### 1.0",
+            "Is 29.0 - 478.2 = 34.452 correct? #### 34.452",
+            "Test text -34*65=80 #### 80",
+            "Test 12-12 = -1 wrong? #### ",
+            "Calculation: calculation calculation",
+            "Testing long 19,203 #### 19,203",
+            "Testing long 19,203,923 #### 19,203,923"
         ]
 
         mse = self.metrics_xval.calculate_result_mse(predictions, labels)
-        expected_mse = np.array([(4 - 1)**2, 0, (78 - 80)**2, (0 - 1)**2, np.nan])
+        expected_mse = np.array([(5 - 7)**2,(4 - 1)**2, (-4 - 1)**2, 0, (78 - 80)**2, np.nan, np.nan, 1, (19203023-19203923)**2])
         np.testing.assert_almost_equal(mse, expected_mse)
 
 
